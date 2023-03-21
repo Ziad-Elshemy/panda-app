c702e8865d94c999d759996b33f7455e1305ac39
ziadElshimy <127307908+ziadElshimy@users.noreply.github.com>
2023-03-21 20:26:54 +0200

    Handling MyAccountFragment before and after login

--- app/src/main/java/com/easy_pro_code/panda/MainActivity.kt
+++ app/src/main/java/com/easy_pro_code/panda/MainActivity.kt
@@ -3,11 +3,19 @@ package com.easy_pro_code.panda
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.FragmentManager
 import com.easy_pro_code.panda.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        val intentFragment = intent.extras?.getBoolean("fragmentToLoad")
+
+//        if (intentFragment==true){
+//
+//        }
+
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
